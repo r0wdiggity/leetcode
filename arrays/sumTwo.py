3d01851ee83840e30d8bb51ae8ea220ad312c9a6
r0wdiggity <r0wdiggity1@gmail.com>
2020-12-20 11:05:04 -0500

    optimized twoSum

--- arrays/sumTwo.py
+++ arrays/sumTwo.py
@@ -1,6 +1,14 @@
 class Solution:
     def twoSum(self, nums, target):
+         """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return (i, j)
+            d[nums[i]] = i
+        
+        for i in range(len(nums)):
+            if (target - nums[i]) in d and d[target - nums[i]] != i:
+                return (i, d[target - nums[i]])
